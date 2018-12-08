31b31fb808a184d82c46475ce0532ebab7bd4231
GOD\Wafi <wafi@outlook.com>
2018-12-08 20:02:37 +1000

    Added some functionalities

--- scripts.js
+++ scripts.js
@@ -1,9 +1,26 @@
 const calculator = document.querySelector('.main-container');
 const keys = calculator.querySelector('.panel');
 
+
 keys.addEventListener('click', e => {
+	
 	if(e.target.matches('button')) {
-		const key = e.target;
 		
+		const action = e.target.getAttribute('data-action');
+		
+		if(action) {
+			console.log("pressed a function!");
+		}
+		else {
+			const currentValue = document.getElementById('display').textContent;
+			const pressedValue = e.target.textContent;
+			
+			if(currentValue === "0") {
+				document.getElementById('display').textContent = pressedValue;
+			} else {
+				document.getElementById('display').textContent = currentValue + pressedValue;
+			}
+			
+		}
 	}
 })
