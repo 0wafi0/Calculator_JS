f7a322ac22911b8e32e6e6e9102c2f99117af74b
GOD\Wafi <wafi@outlook.com>
2018-12-08 19:27:35 +1000

    Quick session. finished interface will be adding functionality

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const calculator = document.querySelector('.main-container');
+const keys = calculator.querySelector('.panel');
+
+keys.addEventListener('click', e => {
+	if(e.target.matches('button')) {
+		const key = e.target;
+		
+	}
+})
